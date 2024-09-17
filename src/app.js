2af5fa5ca6fcf0037a4c3c709bebd9904871af72
js-enjoyer <vasilen.tihin@gmail.com>
2024-09-17 21:46:17 +0300

    add libraries

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import page from '@page/page.mjs'
+import { html, render } from '@lit/lit-html.js'
+import { classMap } from '@lit/directives/class-map.js'
+import { styleMap } from '@lit/directives/style-map.js'
+import { until } from '@lit/directives/until.js'
+
+page('/index.html', homeView)
+
+page.start()
+
+function homeView() {
+    render(html`<h1>Hello World</h1>`, document.body)
+}
